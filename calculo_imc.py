0149e861841700d7976892635732d3df4e387d09
Lukk59 <lucasan07@gmail.com>
2022-05-27 22:31:06 -0300

    adiciona o resultado do imc

--- calculo_imc.py
+++ calculo_imc.py
@@ -3,4 +3,18 @@ peso = float(input("digite o seu peso(kg): "))
 
 imc = peso / (altura ** 2)
 
-print(f"o seu imc é: {imc}")
+print(f"o seu imc é: {imc:.2f}")
+
+
+if imc < 18.5:
+    print('abaixo do peso')
+elif imc <= 24.9:
+    print('peso normal')
+elif imc <= 29.9:
+    print('pré obesidade')
+elif imc <= 34.9:
+    print('obesidade grau 1')
+elif imc <= 39.9:
+    print('obesidade grau 2')
+else:
+    print('obesidade grau 3')
