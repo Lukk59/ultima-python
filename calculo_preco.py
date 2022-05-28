a20b84338df12313d57321d093a5f0a5e5456099
Lukk59 <lucasan07@gmail.com>
2022-05-27 22:56:49 -0300

    adiciona o valor de desconto

--- calculo_preco.py
+++ calculo_preco.py
@@ -3,3 +3,7 @@ preco = float(input('preço: '))
 
 valor_total = quantidade * preco
 print(f'valor total: {valor_total:.2f} R$')
+
+if valor_total > 5000.0:
+    valor_com_desconto = valor_total * 0.9
+    print(f'valor com desconto de 10%: {valor_com_desconto:.2f} R$')
