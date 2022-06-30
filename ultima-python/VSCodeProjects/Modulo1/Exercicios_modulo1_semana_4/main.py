ca36a7188c2a0c77bd41c5ee0b2b524de50e4d4f
Lukk59 <101298498+Lukk59@users.noreply.github.com>
2022-06-29 21:10:47 -0300

    Delete ultima-python/VSCodeProjects directory

--- ultima-python/VSCodeProjects/Modulo1/Exercicios_modulo1_semana_4/main.py
+++
@@ -1,5 +0,0 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
-print(lista[0:5:2])
-
-print(lista[-6])
--- ultima-python/VSCodeProjects/Study/boleanmaybe.py
+++
@@ -1,7 +0,0 @@
-x = True
-y = False
-
-if y or not x == True:
-    print("True")
-else:
-    print("False")
